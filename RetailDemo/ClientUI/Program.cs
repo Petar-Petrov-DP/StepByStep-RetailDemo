598a296f6598102e42db46c122db256fc827bdca
Petar <petar.petrov@datapassion.de>
2022-10-17 16:03:49 +0200

    FEAT: Created the client

--- RetailDemo/ClientUI/Program.cs
+++ RetailDemo/ClientUI/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using NServiceBus;
+
+static async Task AsyncMain()
+{
+    Console.Title = "ClinetUI";
+
+    var endpointConfiguration = new EndpointConfiguration("ClientUI");
+
+    var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+    var endpointInstance = await Endpoint.Start(endpointConfiguration)
+        .ConfigureAwait(false);
+
+    Console.WriteLine("Press Enter to exit...");
+    Console.ReadLine();
+
+    await endpointInstance.Stop()
+    .ConfigureAwait(false);
+}
