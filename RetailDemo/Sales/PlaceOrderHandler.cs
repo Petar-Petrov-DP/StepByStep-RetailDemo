c8504bc08093cab00ec6326625e7caa21de0cb67
Petar <petar.petrov@datapassion.de>
2022-10-20 17:32:06 +0200

    FEAT: Bugs to see NSBus queue and retry

--- RetailDemo/Sales/PlaceOrderHandler.cs
+++ RetailDemo/Sales/PlaceOrderHandler.cs
@@ -9,11 +9,18 @@ namespace Sales
     public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
     {
         static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
-
+        static Random random = new Random();
 
         public Task Handle(PlaceOrder message, IMessageHandlerContext context)
         {
             log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            //throw new Exception("BOOOOM!");
+            if (random.Next(0, 5) == 0)
+            {
+                throw new Exception("Oops");
+            }
+
 
             var orderPlaced = new OrderPlaced
             {
