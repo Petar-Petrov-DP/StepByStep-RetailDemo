bdbcf27dd14b1ab15ab80efeb1e31a3e48c14d19
Petar <petar.petrov@datapassion.de>
2022-10-20 10:19:07 +0200

    FEAT: Added the Messages project to send messages WIP

--- RetailDemo/ClientUI/Program.cs
+++ RetailDemo/ClientUI/Program.cs
@@ -1,5 +1,8 @@
 ﻿
+using Messages.Commands;
 using NServiceBus;
+using NServiceBus.Logging;
+
 
 static async Task AsyncMain()
 {
@@ -9,12 +12,55 @@ static async Task AsyncMain()
 
     var transport = endpointConfiguration.UseTransport<LearningTransport>();
 
+
+    //Start the endpoint
     var endpointInstance = await Endpoint.Start(endpointConfiguration)
         .ConfigureAwait(false);
 
-    Console.WriteLine("Press Enter to exit...");
-    Console.ReadLine();
+    RunLoop(endpointInstance);
+
 
     await endpointInstance.Stop()
     .ConfigureAwait(false);
+}
+
+
+
+static async Task RunLoop(IEndpointInstance endpointInstance)
+{
+    while (true)
+    {
+        log.Info("Press 'P' to place an order, or 'Q' to quit.");
+        var key = Console.ReadKey();
+        Console.WriteLine();
+
+        switch (key.Key)
+        {
+            case ConsoleKey.P:
+                // Instantiate the command
+                var command = new PlaceOrder
+                {
+                    OrderId = Guid.NewGuid().ToString()
+                };
+
+                // Send the command to the local endpoint
+                log.Info($"Sending PlaceOrder command, OrderId = {command.OrderId}");
+                await endpointInstance.SendLocal(command)
+                    .ConfigureAwait(false);
+
+                break;
+
+            case ConsoleKey.Q:
+                return;
+
+            default:
+                log.Info("Unknown input. Please try again.");
+                break;
+        }
+    }
+}
+
+public partial class Program
+{
+    static ILog log = LogManager.GetLogger<Program>();
 }
